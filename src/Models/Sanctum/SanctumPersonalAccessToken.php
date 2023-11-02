50fc5b3ab85e7fe0b7bc6cd3fa3967f7f664f85c
VAOpti <vaopti@gmail.com>
2023-11-02 23:41:05 +0100

    Fix retrieving personal access

--- src/Models/Sanctum/SanctumPersonalAccessToken.php
+++ src/Models/Sanctum/SanctumPersonalAccessToken.php
@@ -7,4 +7,6 @@ use VisionAura\LaravelCore\Models\PersonalAccessToken;
 class SanctumPersonalAccessToken extends PersonalAccessToken
 {
     protected $connection = 'central_db';
+
+    protected $table = 'personal_access_tokens';
 }
