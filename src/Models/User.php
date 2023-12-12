3bbc939782d2c5ce142d54a981975368f595b9f1
VAOpti <vaopti@gmail.com>
2023-12-12 23:20:00 +0100

    Feat: Update core user model
    Trait roles & relations

--- src/Models/User.php
+++ src/Models/User.php
@@ -6,10 +6,13 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use VisionAura\LaravelCore\Interfaces\RelationInterface;
+use VisionAura\LaravelCore\Traits\Relations;
 
-class User extends Authenticatable
+class User extends Authenticatable implements RelationInterface
 {
-    use HasApiTokens, HasUuids, Notifiable;
+    use HasApiTokens, HasUuids, HasRoles, Notifiable, Relations;
 
     protected $connection = 'central_db';
 
